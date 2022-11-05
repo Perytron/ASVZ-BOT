dee2370c859b94b3765f12cd9ecf3ceedda67190
Perytron <perytron@proton.me>
2022-11-05 15:35:59 +0100

    feat: implement configuration file
    Load configuration from command-line arguments or file interchangeably. Update README.md with instructions on how to use the command-line arguments.

--- README.md
+++ README.md
@@ -10,3 +10,47 @@ First off, open your command-line interface of choice, on Windows this would be 
 You should now find yourself in the root directory of [ASVZ-BOT](https://github.com/Perytron/ASVZ-BOT). Next off, call [pip](https://pip.pypa.io/en/stable/installation/) to install the required packages by entering:
 
 `pip install -r requirements.txt`
+
+### 2 Run ASVZ Bot
+#### 2.1 Navigate to Directory
+In order to run your lovely ASVZ bot, you will first need to visit him at home. Open up your command-line interface of choice and use the navigation software called `cd` to pay him a visit.
+
+`cd C:\location\to\your\repository\ASVZ-BOT\src\asvzbot`
+
+#### 2.2 Call ASVZ Bot
+Now, it depends on how you have set up Python. The matter of installing Python would be a scientific paper alone, so please make sure you know how to run a python script from the command-line. If you don't know where to start, compose an [issue](https://github.com/Perytron/ASVZ-BOT/issues) and I will help you as soon as possible™. Mostly, however, it boils down to either one of the following options:
+
+`python main.py` or `python3 main.py` or `"C:/location/to/python.exe" main.py`
+
+#### 2.3 Command-Line Arguments
+In order for your lovely ASVZ bot to work his (...) off, he firstly needs to be well-fed using command-line arguments or a configuration file. Silly jokes aside, be aware that, when running it the first time, you need to provide every parameter using command line arguments. If every required parameter has been provided, the bot will then run and automatically generate a configuration file which is being used the next time. This enables you to completely omit command-line arguments from now on, or use them as a way of updating the configuration file.\
+Please consider running the following command, using the method that works for you, to get an overview over the possible options:
+
+`python main.py -h`
+
+This will generate the following message, telling you what each parameter does in detail:
+
+```
+usage: Parser [-h] [-u USERNAME] [-p PASSWORD] [-b {brave,chromium,chrome,firefox,edge}] [-l {asvz,switch}] [-i ID] [-s]
+
+options:
+  -h, --help            		show this help message and exit
+  -u USERNAME, --username USERNAME	provide username you use to login to ASVZ
+  -p PASSWORD, --password PASSWORD	provide password to the corresponding username
+  -b {...}, --browser {...}		provide if you want to use a specific browser
+  -l {...}, --login {...}		provide login method
+  -i ID, --id ID        		provide sports lesson id
+  -s, --stealth         		do not store the password in 'config.json'
+```
+#### 2.4 Examples
+A complete first call for a newly cloned bot would look like this:
+
+`python main.py -u UZHBeschte -p ETHIschScheiseLoL -b firefox -l switch -i 69420`
+
+The following example would be a call if some info has changed: For example, you have changed your password and additionally, you now would like to participate in another sports lesson. Your lovely bot will read the configuration and override it with the command-line options you have provided. In this particular example, the username (-u), the browser (-b) and the login method (-l) would stay the same, but the password (-p) and the sports lesson ID (-i) will be updated.
+
+`python main.py -i 123456 -p HelloWorld`
+
+If you, for whatever reason, don't want your password stored in the configuration file, you can supply `-s` as a parameter. However, be aware that you therefore need to supply the password each time as command-line arguments too.
+
+`python main.py -s -p HelloWorld`
--- src/asvzbot/main.py
+++ src/asvzbot/main.py
@@ -1,6 +1,8 @@
 # Import from Python standard library
 import os
 import argparse
+import json
+from pathlib import Path
 
 # Import self-made additions
 from log import *
@@ -29,7 +31,9 @@ from webdriver_manager.core.utils import ChromeType # Additionally needed for Ch
 # Configurations
 HEADLESS = True # Set this to false if you want to see Selenium do its magic visibly
 BROWSER_SELECTION = ["brave", "chromium", "chrome", "firefox", "edge"]
+LOGIN_SELECTION = ["asvz", "switch"]
 BRAVE_INSTALLATION_PATH = os.path.expandvars(r'%PROGRAMFILES%/BraveSoftware/Brave-Browser/Application/brave.exe')
+CONFIGURATION_FILE = "config.json"
 
 def getDriver(browser=False):
     '''
@@ -55,7 +59,7 @@ def getDriver(browser=False):
         # Find all Firefox options here: https://wiki.mozilla.org/Firefox/CommandLineOptions
         options.add_argument("--incognito")
         options.add_argument('--disable-translate')
-        options.add_argument("--log-level=0") # From which level should error messages appear: INFO = 0,  WARNING = 1, LOG_ERROR = 2, LOG_FATAL = 3.
+        options.add_argument("--log-level=3") # From which level should error messages appear: INFO = 0,  WARNING = 1, LOG_ERROR = 2, LOG_FATAL = 3.
         options.add_experimental_option('excludeSwitches', ['enable-logging']) # Disable "DevTools listening (...)"
         options.add_experimental_option("prefs", {"intl.accept_languages": "de"})
     else:
@@ -88,6 +92,69 @@ def getDriver(browser=False):
         logger.info(browser[0].upper() + browser[1:] + " instance successfully created")
         return driver
 
+def getConfiguration(arguments, filename):
+    '''
+    Collect necessary configuration either from the configuration file, the command-line arguments or a combination of both and return a dictionary
+    '''
+
+    configuration_requirements = ["username", "password", "browser", "login", "id"] # List holding the options which are required for the script to function properly
+    configuration_collected = {} # Dictionary holding the extracted configuration which will be returned to the script after this functions ends
+    arguments_dictionary = {} # Dictionary holding the converted command-line arguments
+    configuration_json = {} # Dictionary holding the extracted configuration from 'config.json'
+
+    # Convert the required arguments into a dictionary for easy access later on
+    for key in configuration_requirements:
+        try: 
+            argument_value = getattr(arguments, key)
+            if not argument_value is None: arguments_dictionary[key] = argument_value
+        except: continue
+
+    # Check that the configuration file exists
+    if Path(filename).is_file():
+
+        # Read the configuration file and parse its JSON content to a new dictionary
+        with open(filename, "r") as file: 
+            try:
+                configuration_json = json.load(file)
+            except: 
+                logger.error("There was an error parsing '" +  filename + "'. Please either fix or delete the file.")
+                return False
+            
+        # Loop over the newly created JSON dictionary and extract the useful configuration
+        for key in configuration_json.keys():
+            if key in configuration_requirements:
+
+                # Check validity of input (it could be that user manually edits 'config.json')
+                match key:
+                    case "browser":
+                        if not configuration_json[key] in BROWSER_SELECTION: 
+                            logger.warning("Provided browser is invalid. Pick one of the following options: " + str(', '.join(BROWSER_SELECTION)))
+                            continue
+                    case "login":
+                        if not configuration_json[key] in LOGIN_SELECTION: 
+                            logger.warning("Provided login method is invalid. Pick one of the following options: " + str(', '.join(LOGIN_SELECTION)))
+                            continue
+
+                configuration_collected[key] = configuration_json[key]
+    
+    # Loop over all command-line arguments and extract the useful configuration (overrides JSON because command-line is regarded as priority)
+    for key in arguments_dictionary.keys(): configuration_collected[key] = arguments_dictionary[key]
+
+    # Make sure that in the end every requirement has been provided (on either way: command-line arguments or 'config.json')
+    if not sorted(configuration_collected.keys()) == sorted(configuration_requirements):
+        difference = set(configuration_requirements) - set(configuration_collected.keys()) # Retrieve elements that should be extracted but were not found on either way
+        logger.error("You have forgot to feed ASVZ-bot with the following value(s): " + str(', '.join(difference)))
+        return False
+    
+    # Store the configuration in the JSON file for later usage (don't store password if '-s' / '--stealth' has been provided)
+    with open(filename, "w") as file:
+        output_directory = configuration_collected.copy()
+        if arguments.stealth: del output_directory["password"]
+        json.dump(output_directory, file, sort_keys=True, indent=4)
+        
+    return configuration_collected
+
+
 def main():
 
     # Clear the console before each new run
@@ -97,42 +164,39 @@ def main():
     parser = argparse.ArgumentParser("Parser")
 
     # Configure command-line argument parser (argument is followed by variable)
-    parser.add_argument("-u", "--username", type=str, help="provide the username you use to login to ASVZ")
-    parser.add_argument("-p", "--password", type=str, help="provide the password to the corresponding username")
+    parser.add_argument("-u", "--username", type=str, help="provide username you use to login to ASVZ")
+    parser.add_argument("-p", "--password", type=str, help="provide password to the corresponding username")
     parser.add_argument("-b", "--browser", type=str, help="provide if you want to use a specific browser", choices=BROWSER_SELECTION)
+    parser.add_argument("-l", "--login", type=str, help="provide login method", choices=LOGIN_SELECTION)
+    parser.add_argument("-i", "--id", type=int, help="provide sports lesson id")
 
     # Configure command-line argument parser (argument has meaning by just providing it)
-    parser.add_argument("--switch", help="add if you login with SWITCH EDU", action="store_true")
-    parser.add_argument("--asvz", help="add if you login with ASVZ ID", action="store_true")
+    parser.add_argument("-s", "--stealth", help="do not store the password in 'config.json'", action="store_true")
 
     # Convert all arguments (unused ones too) into an 'Namespace' object, from which the arguments can be accessed programmatically
-    args = parser.parse_args()
+    arguments = parser.parse_args()
 
-    # Check that only either SWITCH EDU or ASVZ ID is set
-    if args.switch and args.asvz: 
-        logger.error("Provide only one login method: Either pass '--switch' or '--asvz' as an argument, depending on your ASVZ login method. Do not pass both.")
-        return
-    if not args.switch and not args.asvz: 
-        logger.error("Provide a valid login method: Either pass '--switch' or '--asvz' as an argument, depending on your ASVZ login method.")
-        return
+    # Collect configurations from either the configuration file, the command-line options or a combination of both
+    configuration = getConfiguration(arguments, CONFIGURATION_FILE)
 
-    # Create new browser instance (if a specific browser has been provided)
-    driver = False
-    if not args.browser is None:
-        driver = getDriver(args.browser)
+    # If the configuration could not retreive all reqired values, exit the script
+    if not configuration: return
+
+    # Create new browser instance
+    driver = getDriver(configuration["browser"])
     
     # getDriver() returns 'False' if no driver could be created, so this if-statement triggers when the desired browser fails or none has been tested yet
-    if driver == False:
+    if not driver:
         for browser in BROWSER_SELECTION:
             driver = getDriver(browser)
             if not driver == False: break
     
     # If still no browser could be found, the user does not have a compatible browser installed
-    if driver == False:
+    if not driver:
         logger.error("No compatible browser could be found. Please install a supported browser: " + str(BROWSER_SELECTION)[1:-1])
         return
 
-    # Get sample website and print it's HTML source
+    # Get sample website and print its HTML source
     driver.get("https://api.perytron.ch/ARES.php?x=10000&y=10000")
     driver.implicitly_wait(3)
     print (driver.page_source.encode("utf-8"))
